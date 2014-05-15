5416775559744a0b58f74ddbc41a4c04dd3256ee
trinhkethanh <trinh.kethanh@gmail.com>
2014-05-15 23:29:56 +0700

    Comment Author File System

--- modules/config/models/LetConfig.php
+++ modules/config/models/LetConfig.php
@@ -3,7 +3,7 @@
  * @link http://www.letyii.com/
  * @copyright Copyright (c) 2014 Let.,ltd
  * @license https://github.com/letyii/cms/blob/master/LICENSE
- * @author Ngua Go <nguago@let.vn>, Caravan Of Life <trinh.kethanh@gmail.com>
+ * @author Ngua Go <nguago@let.vn>, Trinh Ke Thanh <trinh.kethanh@gmail.com>
  */
 
 namespace app\modules\config\models;
