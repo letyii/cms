c346cc6bfd82962a82411932cbc22e0c6cfb3bb4
Cong TS <congts@smartosc.com>
2014-05-25 14:45:25 +0700

    update

--- themes/letyii/modules/member/views/backend/rbac/item.php
+++ themes/letyii/modules/member/views/backend/rbac/item.php
@@ -6,8 +6,8 @@ $this->registerJsFile(\yii\helpers\Url::base() . '/modules/member/assets/js/memb
 
 <!-- Nav tabs -->
 <ul class="nav nav-tabs">
-    <li><?php Html::a('Cây quan hệ vai trò', ['backend/rbac/index']) ?></li>
-    <li class="active"><?php Html::a('Vai trò và phân quyền', ['backend/rbac/item']) ?></li>
+    <li><?php echo Html::a('Cây quan hệ vai trò', ['backend/rbac/index']) ?></li>
+    <li class="active"><?php echo Html::a('Vai trò và phân quyền', ['backend/rbac/item']) ?></li>
 </ul>
 
 <div class="panel panel-default" style="border-top: 0;"> <!-- Chinh lai sau -->
